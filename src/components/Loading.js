395379062fd1d3124ffa721098483f6e8c2ac8c9
Prakriti Pokharel <iprakritip@gmail.com>
2023-06-15 21:06:26 +0545

    add loading animation

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Loading() {
     return (
-        <p>Waiting......</p>
+        <div className='flex ml-[25rem] w-[100vw] h-[100vh] bg-slate-100'>
+            <div class="loadingio-spinner-reload-gg1v0d3u91o"><div class="ldio-7i9qtyio88o">
+                <div><div></div><div></div><div></div></div>
+            </div></div>
+        </div>
     )
 }
