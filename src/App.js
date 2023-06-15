d52f1b83e908e796c2c0546bc501542663470f77
Prakriti Pokharel <iprakritip@gmail.com>
2023-06-15 14:43:32 +0545

    link logo to homepage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import CharacterDetails from "./pages.js/CharacterDetails";
 import CharactersList from "./pages.js/CharactersList";
 import Search from "./pages.js/Search";
@@ -7,9 +7,12 @@ import Search from "./pages.js/Search";
 function App() {
   return (
     <div className="App py-1 px-2 lg:px-[6rem] lg:py-[1rem] bg-slate-100">
+      <Link to='/'>
       <div className="h-[3rem] w-[3rem] lg:h-[6rem] lg:w-[6rem]">
+        
         <img src="https://www.freepnglogos.com/uploads/rick-and-morty-png/rick-and-morty-portal-moon-mod-download-35.png" alt="logo" />
       </div>
+      </Link>
       <Routes>
         <Route path='/' element={<CharactersList />} />
         <Route path='/search' element={<Search />} />
