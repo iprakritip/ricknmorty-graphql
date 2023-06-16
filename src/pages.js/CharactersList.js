27455384896675abde24a2f9b221e20bca1f4422
Prakriti Pokharel <iprakritip@gmail.com>
2023-06-16 11:52:11 +0545

    check for data before loading locations

--- src/pages.js/CharactersList.js
+++ src/pages.js/CharactersList.js
@@ -1,25 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 import Character from '../components/Character';
 import Fallback from '../components/Fallback';
 import { useCharacters } from '../hooks/useCharacters';
+import SearchBar from '../components/SearchBar'
 
 export default function CharactersList() {
     const { error, loading, data } = useCharacters();
     // console.log(loading, error, data);
-
+    const [name, setName] = useState('');
     return (
-        <div>
+        <div className="flex flex-col gap-6">
             <Link className=' my-4 mr-5 lg:mr-[2rem] flex justify-end' to='/search'>
                 <button className='goToSearch px-2 py-1 border border-dotted rounded-lg border-green-500 text-green-500'>{`Search Locations by character`}</button>
             </Link>
+            <SearchBar label='Search character' placeholder="e.g: Rick Sanchez" value={name} onChange={(e) => setName(e.target.value)} />
             {data ?
                 <div className='characterList flex flex-wrap justify-center gap-8 bg-slate-100'>
                     {data?.characters.results.map(character => {
                         return <Character character={character} />
                     })}
                 </div>
-                : <Fallback loading={loading ? true : false} error={error ? true : false}/>}
+                : <Fallback loading={loading ? true : false} error={error ? true : false} />}
 
         </div>
     )
--- src/pages.js/Search.js
+++ src/pages.js/Search.js
@@ -14,11 +14,11 @@ export default function Search() {
     useEffect(() => {
         name !== '' ? setButtonDisability(false) : setButtonDisability(true)
     }, [name])
-    
+
     return (
         <div className=' bg-slate-100 flex h-[68vh] flex-col gap-10 mt-[5rem]'>
             <div className='flex flex-col gap-6 items-center w-full'>
-                <SearchBar onChange={(e) => setName(e.target.value)} name={name} />
+                <SearchBar onChange={(e) => setName(e.target.value)} name={name} label="Search for a character's locations" placeholder="e.g: Rick Sanchez" />
                 <button disabled={buttonDisability} className={`${buttonDisability ? 'bg-green-300' : 'bg-green-500'} px-4 py-1 border rounded-lg`} onClick={() => {
                     getLocations({
                         variables: {
@@ -29,7 +29,7 @@ export default function Search() {
                 }}>Search</button>
             </div>
             <div className='bg-slate-100'>
-                {filteredBy && <h2 className='font-bold text-green-500'>{data ? `${filteredBy}'s Locations` : `There is no character by the name '${filteredBy}'. `}</h2>}
+                {filteredBy && <h2 className='font-bold text-xl text-green-500'>{data?.characters?.results.length ? `${filteredBy}'s Locations` : `There is no character by the name '${filteredBy}'. `}</h2>}
                 {data?.characters?.results.map(character => {
                     return <div>{data
                         ? <Location location={character.location.name} />
