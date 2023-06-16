04d5ae0e1710e46c84c1b9ce1e2f9e82c960f6bd
Prakriti Pokharel <iprakritip@gmail.com>
2023-06-16 14:03:22 +0545

    filter characters by name on home pagae search

--- src/pages.js/CharactersList.js
+++ src/pages.js/CharactersList.js
@@ -7,21 +7,31 @@ import SearchBar from '../components/SearchBar'
 
 export default function CharactersList() {
     const { error, loading, data } = useCharacters();
-    // console.log(loading, error, data);
+
     const [name, setName] = useState('');
+    const filtered = name && data?.characters.results.filter((character) => {
+        return character.name.toLowerCase().includes((name.toLowerCase()))
+    })
+    // console.log(data?.characters.results);
     return (
         <div className="flex flex-col gap-6">
             <Link className=' my-4 mr-5 lg:mr-[2rem] flex justify-end' to='/search'>
                 <button className='goToSearch px-2 py-1 border border-dotted rounded-lg border-green-500 text-green-500'>{`Search Locations by character`}</button>
             </Link>
             <SearchBar label='Search character' placeholder="e.g: Rick Sanchez" value={name} onChange={(e) => setName(e.target.value)} />
-            {data ?
-                <div className='characterList flex flex-wrap justify-center gap-8 bg-slate-100'>
-                    {data?.characters.results.map(character => {
+            {!data
+                ? <Fallback loading={loading ? true : false} error={error ? true : false} />
+                : !name
+                    ? <div className='characterList flex flex-wrap justify-center gap-8 bg-slate-100'>
+                        {data?.characters.results.map(character => {
+                            return <Character character={character} />
+                        })}
+                    </div>
+                    : <div>{filtered && filtered.map(character => {
+                        // console.log(character);
                         return <Character character={character} />
-                    })}
-                </div>
-                : <Fallback loading={loading ? true : false} error={error ? true : false} />}
+                    })}</div>
+            }
 
         </div>
     )
